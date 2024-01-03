082d6c1d694b55c2e3fa0a575a09f0c44be8bd56
Karina Pospos <114627942+Kary1661@users.noreply.github.com>
2024-01-03 19:47:49 -0300

    Add files via upload

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,54 @@
+const { DataTypes, INTEGER, STRING, UUIDV4 } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: STRING,
+      allowNull: false,
+    },
+    image: {
+      type: STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    launchDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    rating: {
+      type: DataTypes.DECIMAL(3, 2),
+      allowNull: false
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    }
+  }, {
+    timestamps: false
+  });
+  sequelize.define('genres', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: UUIDV4,
+    },
+    name: {
+      type: STRING,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false
+  }
+  )
+}
